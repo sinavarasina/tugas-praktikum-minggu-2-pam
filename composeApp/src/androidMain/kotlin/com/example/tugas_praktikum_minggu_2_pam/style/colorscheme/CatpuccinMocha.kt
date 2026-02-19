d604a359e0ec4ff0cc97b54d6b70ad49b1aa3f1f
Varasina Farmadani <sina@sinanonym.my.id>
2026-02-19 22:11:18 +0700

    feat(colorscheme): add Catpuccin Mocha colorscheme
    Signed-off-by: Varasina Farmadani <sina@sinanonym.my.id>

---
+++ composeApp/src/androidMain/kotlin/com/example/tugas_praktikum_minggu_2_pam/style/colorscheme/CatpuccinMocha.kt
@@ -0,0 +1,32 @@
+package com.example.tugas_praktikum_minggu_2_pam.style.colorscheme
+
+import androidx.compose.ui.graphics.Color
+
+object CatpuccinMocha {
+    val rosewater = Color(0xFFF5E0DC)
+    val flamingo = Color(0xFFF2CDCD)
+    val pink = Color(0xFFF5C2E7)
+    val mauve = Color(0xFFCBA6F7)
+    val red = Color(0xFFF38BA8)
+    val maroon = Color(0xFFEBA0AC)
+    val peach = Color(0xFFFAB387)
+    val yellow = Color(0xFFF9E2AF)
+    val green = Color(0xFFA6E3A1)
+    val teal = Color(0xFF94E2D5)
+    val sky = Color(0xFF89DCEB)
+    val sapphire = Color(0xFF74C7EC)
+    val blue = Color(0xFF89B4FA)
+    val lavender = Color(0xFFB4BEFE)
+    val text = Color(0xFFCDD6F4)
+    val subtext1 = Color(0xFFBAC2DE)
+    val subtext0 = Color(0xFFA6ADC8)
+    val overlay2 = Color(0xFF9399B2)
+    val overlay1 = Color(0xFF7F849C)
+    val overlay0 = Color(0xFF6C7086)
+    val surface2 = Color(0xFF585B70)
+    val surface1 = Color(0xFF45475A)
+    val surface0 = Color(0xFF313244)
+    val base = Color(0xFF1E1E2E)
+    val mantle = Color(0xFF181825)
+    val crust = Color(0xFF11111B)
+}
