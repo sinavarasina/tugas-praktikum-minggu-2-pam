4ae2f449c3bfebe3490c3fdca124dc2d00be4234
Varasina Farmadani <sina@sinanonym.my.id>
2026-02-19 21:21:35 +0700

    feat(data): add NewsDataFlow to data/ for emiting new news
    Signed-off-by: Varasina Farmadani <sina@sinanonym.my.id>

---
+++ composeApp/src/androidMain/kotlin/com/example/tugas_praktikum_minggu_2_pam/data/NewsDataFlow.kt
@@ -0,0 +1,19 @@
+package com.example.tugas_praktikum_minggu_2_pam.data
+
+import com.example.tugas_praktikum_minggu_2_pam.model.News
+import com.example.tugas_praktikum_minggu_2_pam.model.NewsFactory
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class NewsDataFlow {
+    fun getNewsStream(): Flow<News> = flow {
+        var currentId = 1
+        while (true) {
+            delay(2000)
+            var newNews = NewsFactory.generateNews(currentId)
+            emit(newNews)
+            currentId++
+        }
+    }
+}
